c8d745fe720aad2b804d9bfa305fbdc28117b0b8
MykhailoShamrai <99137234+MykhailoShamrai@users.noreply.github.com>
2024-11-02 21:17:53 +0100

    Update environment.prod.ts

--- wypozyczalnia.client/src/environments/environment.prod.ts
+++ wypozyczalnia.client/src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
     production: true,
-<<<<<<< HEAD
     apiBaseUrl: 'https://rentalapi-esauh2huedhcc2a6.polandcentral-01.azurewebsites.net/api'
-=======
-    apiUrl: 'https://rentalapi-esauh2huedhcc2a6.polandcentral-01.azurewebsites.net'
->>>>>>> 7f704689f3feccc091f450f8c5ce1ef9c7d7763d
   };
