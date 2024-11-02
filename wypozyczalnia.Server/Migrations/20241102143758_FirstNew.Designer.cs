2fabf2b3e573e2d89d647e89482a24707a7ad9e6
MykhailoShamrai <99137234+MykhailoShamrai@users.noreply.github.com>
2024-11-02 15:43:56 +0100

    test (#4)

--- wypozyczalnia.Server/Migrations/20241102143758_FirstNew.Designer.cs
+++ wypozyczalnia.Server/Migrations/20241102143758_FirstNew.Designer.cs
@@ -11,8 +11,8 @@ using wypozyczalnia.Server.Models;
 namespace wypozyczalnia.Server.Migrations
 {
     [DbContext(typeof(VehiclesContext))]
-    [Migration("20241101221346_Second")]
-    partial class Second
+    [Migration("20241102143758_FirstNew")]
+    partial class FirstNew
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -36,17 +36,41 @@ namespace wypozyczalnia.Server.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(100)");
 
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(500)");
+
                     b.Property<string>("Model")
                         .IsRequired()
                         .HasColumnType("nvarchar(100)");
+
+                    b.Property<string>("RegistryNo")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(50)");
+
+                    b.Property<string>("RentalFrom")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(10)");
+
+                    b.Property<string>("RentalTo")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(10)");
 
                     b.Property<string>("SerialNo")
                         .IsRequired()
                         .HasColumnType("nvarchar(100)");
 
+                    b.Property<string>("Type")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(50)");
+
                     b.Property<string>("VinId")
                         .IsRequired()
                         .HasColumnType("nvarchar(100)");
+
+                    b.Property<string>("YearOfProduction")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(4)");
 
                     b.HasKey("CarId");
 
