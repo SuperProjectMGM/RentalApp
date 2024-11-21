40de439184f5ea859d8c741b26332ce9c2ca8634
Mykhailo Shamrai <mike.shamrai@gmail.com>
2024-11-21 14:10:17 +0100

    Changed migration and data types

--- wypozyczalnia.Server/Migrations/20241121130617_ChangedTypesForRental.Designer.cs
+++ wypozyczalnia.Server/Migrations/20241121130617_ChangedTypesForRental.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using wypozyczalnia.Server.Models;
 
 #nullable disable
 
-namespace wypozyczalnia.Server.Migrations
+namespace wypozyczalnia.Server.Migrations.Rentals
 {
     [DbContext(typeof(RentalsContext))]
-    partial class RentalsContextModelSnapshot : ModelSnapshot
+    [Migration("20241121130617_ChangedTypesForRental")]
+    partial class ChangedTypesForRental
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ wypozyczalnia.Server/Migrations/20241121130617_ChangedTypesForRental.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wypozyczalnia.Server.Migrations.Rentals
+{
+    /// <inheritdoc />
+    public partial class ChangedTypesForRental : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalNumber",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LicenceNumber",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateOfReceiptOfDrivingLicense",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BirthDate",
+                table: "Rentals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonalNumber",
+                table: "Rentals",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LicenceNumber",
+                table: "Rentals",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfReceiptOfDrivingLicense",
+                table: "Rentals",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Rentals",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
